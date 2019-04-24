53f9823abb126b92cd1416b455ffa41e9e427e9e
ocean <ocean@localhost.localdomain>
2019-04-24 15:16:13 +0100

    first commit

--- src/components/catalogue/catalogue.component.ts
+++ src/components/catalogue/catalogue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Writer } from './../writer';
 
 @Component({
   selector: 'app-catalogue',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalogue.component.css']
 })
 export class CatalogueComponent implements OnInit {
+
+  genres = [
+    new Writer('./../../assets/images/cartoon-man2.png', 'Nelsom Mandingo', 'Biography'),
+    new Writer('./../../assets/images/cartoon-man.png', 'Isaac Asimov', 'Science fiction'),
+    new Writer('./../../assets/images/cartoon-woman2.png', 'Jane Fonder', 'Romance'),
+    new Writer('./../../assets/images/cartoon-man2.png', 'Pero Dodochie', 'Horror'),
+    new Writer('./../../assets/images/cartoon-woman.png', 'Chidanda Michie', 'Biography'),
+    new Writer('./../../assets/images/cartoon-man.png', 'Thomas Fry', 'Science fiction'),
+    new Writer('./../../assets/images/cartoon-woman2.png', 'Spanish fly', 'Romance'),
+    new Writer('./../../assets/images/cartoon-man2.png', 'Bukola Sarafin', 'Horror'),
+  ];
 
   constructor() { }
 
--- src/components/page/page.component.ts
+++ src/components/page/page.component.ts
@@ -12,6 +12,10 @@ export class PageComponent implements OnInit {
     new Writer('./../../assets/images/cartoon-woman2.png', 'Jane Vagina', 'Romance'),
     new Writer('./../../assets/images/cartoon-man2.png', 'Pero Dodochie', 'Horror'),
     new Writer('./../../assets/images/cartoon-woman.png', 'Chidanda Michie', 'Biography'),
+    new Writer('./../../assets/images/cartoon-man.png', 'Muhammad Busari', 'History'),
+    new Writer('./../../assets/images/cartoon-woman2.png', 'John Doe', 'Horror'),
+    new Writer('./../../assets/images/cartoon-man2.png', 'Dino Meniscus', 'Horror'),
+    new Writer('./../../assets/images/cartoon-woman.png', 'Aisha Bamabama', 'Romance'),
   ];
 // myGenre = this.genres[0];
   constructor() {}
