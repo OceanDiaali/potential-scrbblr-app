0cbbf6626743351b9637d46bc2e301e8d81367c3
ocean <ocean@localhost.localdomain>
2019-04-24 09:45:58 +0100

    add routes and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { CatalogueSummaryComponent } from 'src/components/catalogue-summary/catalogue-summary.component';
+import { CatalogueComponent } from 'src/components/catalogue/catalogue.component';
+import { PageComponent } from './../components/page/page.component';
+
+import { PageNotFoundComponent } from './../components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', component: CatalogueSummaryComponent},
+  { path: 'catalogue-summary', component: CatalogueSummaryComponent },
+  { path: 'catalogue',      component: CatalogueComponent },
+  { path: 'page', component: PageComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [
+    CatalogueSummaryComponent,
+    CatalogueComponent,
+    PageComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
